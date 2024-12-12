e5f062cbbbe96a004859bbd877260e2469e9f021
Sergey Li <sergey@mellowingfactory.com>
2024-12-12 17:25:33 +0900

    - huge UI lags : did i fix it?

--- amoring/UI/Session/Amoring/AmoringView.swift
+++ amoring/UI/Session/Amoring/AmoringView.swift
@@ -19,51 +19,71 @@ struct AmoringView: View {
             ZStack {
                 if amoringController.checkIn != nil {
                     ProfilesView(selectedIndex: $selectedIndex)
-                        .navigationBarItems(leading:
-                                                Text("AMORING")
-                            .font(bold20Font)
-                            .foregroundColor(.yellow300)
-                                            , trailing:
-                                                HStack {
-                            Text(amoringController.countDown.toString())
-                                .font(medium16Font)
-                                .foregroundColor(.yellow300)
-                                .fixedSize(horizontal: true, vertical: false)
-                                .lineLimit(1)
-                            Button(action: {
-                                showAlert = true
-                            }) {
-                                Image("ic-leave-room")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: Size.w(32), height: Size.w(32))
-                            }
-                            .alert(isPresented: $showAlert) {
-                                Alert(title: Text("체크아웃하기"),
-                                      message: Text("라운지 체크아웃 시\n프로필이 더 이상 소개되지 않습니다.\n라운지에서 체크아웃 하시겠습니까?"),
-                                      primaryButton: .cancel(Text("취소")), secondaryButton: .default(Text("확인"), action: leave))
-                            }
-                        }
-                        )
-                } else {
-                    CheckInView()
-                        .toolbar {
-                            ToolbarItem(placement: .principal) {
-                                Text("AMORING")
-                                    .font(bold20Font)
-                                    .foregroundColor(.yellow300)
-                            }
-                        }
-//                        .navigationBarItems(
-//                            trailing: Button(action: {
-//                                showInfo.toggle()
+//                        .navigationBarItems(leading:
+//                                                Text("AMORING")
+//                            .font(bold20Font)
+//                            .foregroundColor(.yellow300)
+//                                            , trailing:
+//                                                HStack {
+//                            Text(amoringController.countDown.toString())
+//                                .font(medium16Font)
+//                                .foregroundColor(.yellow300)
+//                                .fixedSize(horizontal: true, vertical: false)
+//                                .lineLimit(1)
+//                            Button(action: {
+//                                showAlert = true
 //                            }) {
-//                                Image("ic-info")
+//                                Image("ic-leave-room")
 //                                    .resizable()
 //                                    .scaledToFit()
 //                                    .frame(width: Size.w(32), height: Size.w(32))
 //                            }
+//                            .alert(isPresented: $showAlert) {
+//                                Alert(title: Text("체크아웃하기"),
+//                                      message: Text("라운지 체크아웃 시\n프로필이 더 이상 소개되지 않습니다.\n라운지에서 체크아웃 하시겠습니까?"),
+//                                      primaryButton: .cancel(Text("취소")), secondaryButton: .default(Text("확인"), action: leave))
+//                            }
+//                        }
 //                        )
+                } else {
+                    CheckInView()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("AMORING")
+                        .font(bold20Font)
+                        .foregroundColor(.yellow300)
+                        .opacity(amoringController.checkIn == nil ? 1 : 0)
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("AMORING")
+                        .font(bold20Font)
+                        .foregroundColor(.yellow300)
+                        .opacity(amoringController.checkIn == nil ? 0 : 1)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack {
+                        Text(amoringController.countDown.toString())
+                            .font(medium16Font)
+                            .foregroundColor(.yellow300)
+                            .fixedSize(horizontal: true, vertical: false)
+                            .lineLimit(1)
+                        Button(action: {
+                            showAlert = true
+                        }) {
+                            Image("ic-leave-room")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: Size.w(32), height: Size.w(32))
+                        }
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text("체크아웃하기"),
+                                  message: Text("라운지 체크아웃 시\n프로필이 더 이상 소개되지 않습니다.\n라운지에서 체크아웃 하시겠습니까?"),
+                                  primaryButton: .cancel(Text("취소")), secondaryButton: .default(Text("확인"), action: leave))
+                        }
+                    }
+                    .opacity(amoringController.checkIn == nil ? 0 : 1)
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
